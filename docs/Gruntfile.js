fccaf37bdfe5388d7100133bd2788cc6fec0f4af
Anastasiia Litvina <al@click-solutions.de>
2018-11-09 13:48:56 +0100

    import prototype

---
+++ docs/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        sourcemap: false
+      },
+      dist: {
+        files: {
+          'public_html/css/custom.css' : 'scss/main.scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+        sourcemap: false
+      },
+      css: {
+        files: ['scss/**/*.scss'],
+        tasks: ['sass']
+      },
+    },
+  });
+  require('load-grunt-tasks')(grunt);
+  grunt.registerTask('default', ['sass', 'watch']);
+
+}
