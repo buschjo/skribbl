ed3ee0239048a54d1dab141d125e64765c9e4864
Anastasiia Litvina <al@click-solutions.de>
2018-11-18 23:23:43 +0100

    add second model

--- docs/js/index.js
+++ docs/js/index.js
@@ -249,7 +249,7 @@ function getClassNames(indices) {
 load the class names 
 */
 async function loadDict() {
-    loc = 'model/class_names.txt'
+    loc = 'model2/class_names.txt'
     
     await $.ajax({
         url: loc,
@@ -326,7 +326,7 @@ async function start(cur_mode) {
     mode = cur_mode;
     
     //load the model 
-    model = await tf.loadModel('model/model.json');
+    model = await tf.loadModel('model2/model.json');
     
     //warm up 
     model.predict(tf.zeros([1, 28, 28, 1]));
