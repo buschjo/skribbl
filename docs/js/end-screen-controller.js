946800f3f649d9b6710ccc91b4cab51010874cbe
Nik Heise <NikHeise@users.noreply.github.com>
2019-02-04 12:05:41 +0100

    Add endscreen without score

--- docs/js/end-screen-controller.js
+++ docs/js/end-screen-controller.js
@@ -41,7 +41,7 @@ class EndScreenController extends ViewController {
         } else {
             res.innerHTML = "<h1>TIME'S UP!</h1>" +
                 '<img src="assets/defeat_logo.png" alt="defeat logo">' +
-                "<p>Oh no, I couldn't guess it! " + 'I was <span class="bold">' + Math.round(this.appController.modelData.probs[0] * 100) + '%</span> sure that the word was <span class="bold">' + this.appController.modelData.names[0] + '</span></p>';
+                "<div><p>Oh no, I couldn't guess it! " + 'I was <span class="bold">' + Math.round(this.appController.modelData.probs[0] * 100) + '%</span> sure that the word was <span class="bold">' + this.appController.modelData.names[0] + '</span></p></div>';
             //"<p>Clear-Button used: " + this.appController.gameRound.canvasData.clearCounter + "</p>" +
             //"<p>Undo-Button used: " + this.appController.gameRound.canvasData.undoCounter + "</p>" +
             //"<p>Finger lifted: " + this.appController.gameRound.canvasData.fingerLiftedCounter + "</p>";
@@ -62,12 +62,13 @@ class EndScreenController extends ViewController {
 
     //show victory screen with victory specific information
     callVictoryScreen(res, resButton) {
-        res.innerHTML = '<img src="assets/win_logo.png" alt="victory logo">' +
-            '<p>Yeah, I guessed it! Good Draw! </p><p> I was <span id="word-score">' + Math.round(this.appController.modelData.probs[0] * 100) + '%</span> sure and it was on the first place!</p>' +
-            "<p>You needed " + this.appController.scores.timeElapsed.toFixed(2) + " seconds.</p>" +
-            "<p>Clear-Button used: " + this.appController.scores.stats.clear + "</p>" +
-            "<p>Undo-Button used: " + this.appController.scores.stats.undo + "</p>" +
-            "<p>Finger lifted: " + this.appController.scores.stats.fingerLifted + "</p>";
+        res.innerHTML = "<h1>YOU WON!</h1>" +
+            '<img src="assets/win_logo.png" alt="victory logo">' +
+            '<div><p>Yeah, I guessed it! Good Draw! I was <span id="word-score">' + Math.round(this.appController.modelData.probs[0] * 100) + '%</span> sure and it was on the first place!</p>' +
+            '<p>You needed <span class="bold">' + this.appController.scores.timeElapsed.toFixed(2) + ' seconds</span>.</p></div>';
+            // "<p>Clear-Button used: " + this.appController.scores.stats.clear + "</p>" +
+            //"<p>Undo-Button used: " + this.appController.scores.stats.undo + "</p>" +
+            //"<p>Finger lifted: " + this.appController.scores.stats.fingerLifted + "</p>";
         //"<p>" + this.appController.gameRound.word + "</p>";
 
         resButton.innerText = 'NEXT';
